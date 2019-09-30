86307fb728164547b34d0565ea9b3fbee72298fd
Lorna Jane Mitchell <lorna.mitchell@vonage.com>
2019-09-30 07:32:21 +0100

    Switch to basic spectral validation of built-in rules (#172)

--- bin/validate-specs.sh
+++ bin/validate-specs.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 HAS_FAILURE=0
 
-COMMAND="speccy lint"
-# We don't have tags in every OAS
-COMMAND="$COMMAND -s openapi-tags"
-# Some operations don't need to be grouped
-COMMAND="$COMMAND -s operation-tags"
-# Sometimes we set default and example to the same value. We should fix this in the future
-COMMAND="$COMMAND -s default-and-example-are-redundant"
-# Our tag order is respected when rendering in the sidebar. We don't want them alphabetical
-COMMAND="$COMMAND -s openapi-tags-alphabetical"
-# There's an issue that means file-based refs trigger this rule incorrectly
-COMMAND="$COMMAND -s reference-no-other-properties"
+COMMAND="spectral lint -v"
 
 DOCS=$1
 
